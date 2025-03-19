028b709edb14be617d154a961f93ac0174b21692
oslo-c <oslos_lot@proton.me>
2025-03-19 06:43:08 -0700

    Updated Imports

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
+import os
 from setuptools import setup
+
+# Read the contents of README.md for a long description (if available)
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
 
 setup(
     name='ghostmap',
     version='1.0.0',
     description='GhostMap: Mapping the Digital Shadows',
-    author='Your Name',
-    author_email='youremail@example.com',
-    py_modules=['ghostmap'],  # assumes your main script is ghostmap.py
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='oslo-c4',
+    author_email='oslo-c4@proton.me',
+    py_modules=['ghostmap'],  # Assumes your main script is ghostmap.py
+    python_requires='>=3.6',
     install_requires=[
-        'requests',
+        'requests>=2.20.0',  # Automatically installs requests if not present
     ],
     entry_points={
         'console_scripts': [
             'ghostmap=ghostmap:main',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
